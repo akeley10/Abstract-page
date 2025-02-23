ced9baf9d5e93a6bbb219854086b8344b89636f9
Alejandro Martel <91066025+akeley10@users.noreply.github.com>
2025-02-23 20:44:25 +0000

    añadiendo una página nueva con la bsuqueda del input y añadiendole estilos del home a la página search y enlaces

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const menuIcon = document.getElementById('menuIcon');
+    const formQuery = document.getElementById('formQuery').value;
+    const query = document.getElementById('query').value;
+    
+
+    formQuery.addEventListener('onsubmit',()=>{
+        event.preventDefault();
+
+        console.log(query);
+
+    });
 
 
     menuIcon.addEventListener('click',()=>{
