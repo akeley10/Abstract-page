5d550d272dfbbeda193fbf1418685c497f55748d
Alejandro Martel <91066025+akeley10@users.noreply.github.com>
2025-02-24 09:50:25 +0000

    organizando js del index.js

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const menuIcon = document.getElementById('menuIcon');
-    const formQuery = document.getElementById('formQuery').value;
-    const query = document.getElementById('query').value;
-    
-
-    formQuery.addEventListener('onsubmit',()=>{
-        event.preventDefault();
-
-        console.log(query);
-
-    });
-
 
     menuIcon.addEventListener('click',()=>{
         document.body.classList.toggle('menu-visible');
